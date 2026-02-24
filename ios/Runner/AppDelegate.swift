b0814dfa87f10776e416f0ee8ed0bb6311e76726
SiaJiaLe <22062566@imail.sunway.edu.my>
2026-02-24 22:18:45 +0800

    add android manifest

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,4 @@
-import Flutter
-import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +6,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCvp_aU_cTjZsdI-rmatkz-gorqJ3nozww")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
